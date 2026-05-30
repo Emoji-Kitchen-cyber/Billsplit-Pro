58f9b75402fb22e5b800c5f17c9de42f5a6f0ffc
Emoji-Kitchen-cyber <jahangirgujjar056@gmail.com>
2026-05-30 21:54:53 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'billsplit-v2';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','index.html','css/style.css','js/app.js'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
